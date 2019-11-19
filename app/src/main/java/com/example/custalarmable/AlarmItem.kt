c99b66252b32fec982f5bb9cc710681c1967ce30
Mingjie Zhao <mingjie7@illinois.edu>
2019-11-19 01:24:19 -0600

    first commit

---
+++ app/src/main/java/com/example/custalarmable/AlarmItem.kt
@@ -0,0 +1,9 @@
+package com.example.custalarmable
+
+import java.time.LocalTime
+
+class AlarmItem {
+    var alarmName: String = "My Alarm"
+    var alarmTime: String = "9:00"
+    var alarmAmPm: String = "AM"
+}
